1ca24052b3d9d1fe5c2c485b1482b868182f8acd
keshav00001 <keshavking111@gmail.com>
2024-10-24 23:35:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ImageCrop from "@/components/ImageCrop";
+import ImageCropProvider from "@/providers/ImageCropProvider";
+
+const App = () => {
+  return (
+    <div className="bg-gray-100 h-screen flex justify-center items-center">
+      <ImageCropProvider>
+        <ImageCrop />
+      </ImageCropProvider>
+    </div>
+  );
+};
+
+export default App;
